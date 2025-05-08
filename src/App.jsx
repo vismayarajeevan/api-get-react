b7c3f22f42119075358c3ebb626ea4685ebcafc8
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-05-08 20:46:27 +0530

    get completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  const getData = async()=>{
+    try {
+      const result = await axios.get("https://dummyjson.com/products")
+      console.log(result);
+
+      if(result.status == 200){
+        setData(result.data.products)
+        
+
+      }else{
+        console.log("error for fetching data");
+        
+      }
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+
+ }
+
+ console.log("data",data);
+ 
+
+ useEffect(()=>{
+  getData()
+
+ },[])
 
   return (
     <>
+
+    <div>
+      <p>Products</p>
+
+      {
+        data.length >0 ? (
+          data.map(item=>(
+            <div id={item.id}>
+              <h4>{item.title}</h4>
+              <p>{item.description}</p>
+              </div>
+          ))
+
+        ):(
+          <div>no products...</div>
+        )
+      }
+
+    </div>
      
     </>
   )
